2533c1e7575c535f6bccb58aab81af81db8ffff5
Senai <Senai@CEPEAS21104187>
2023-09-05 21:37:30 -0300

    Implementa entidades de clientes e colaborador parcialmente

---
+++ iMyApp/Application/Negocio/Entidades/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using Negocio.Entidades.Comum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Entidades
+{
+    //clientes herda os dados de pessoas
+    public class Cliente : Pessoa
+    {
+        public DateTime Nascimento { get; set; }
+
+    }
+}
